e4947eea568c2fd6027af3d181b46925d254d2d4
chinosk114514 <74499927+chinosk114514@users.noreply.github.com>
2021-03-30 21:44:12 +0800

    一键更新网址列表

---
+++ 更新列表.py
@@ -0,0 +1,17 @@
+import alioss
+
+aqwq=alioss.readoss('myname','img/path1',False) #false内网 true外网
+
+aqwq=aqwq + "\n" + alioss.readoss('myname','img/path2',False)
+
+aqwq=aqwq + "\n" + alioss.readoss('myname','img/path3',False)
+
+aqwq=aqwq + "\n" + alioss.readoss('myname','img/path4',False)
+
+
+with open("C:\\Users\\Y7000\\Desktop\\sdk_v2.6.5\\alioss\\urls.txt","w",encoding='utf-8') as f:
+    f.write(aqwq)
+
+print("共更新 "+str(alioss.get_tnm()) +"张图片")
+
+
