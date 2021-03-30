6adf8edf89b0b7343a9807ce629a608c0e7167f6
chinosk114514 <74499927+chinosk114514@users.noreply.github.com>
2021-03-30 22:01:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-
+//存有链接的文件名
 $filename = "urls.txt";
 if(!file_exists($filename)){
 	die('文件不存在');
 }
 
+//从文本获取链接
 $pics = [];
 $fs = fopen($filename, "r");
 while(!feof($fs)){
@@ -14,15 +15,18 @@ while(!feof($fs)){
 	}
 }
 
+//从数组随机获取链接
 $pic = $pics[array_rand($pics)];
 
+//返回指定格式
 $type=$_GET['type'];
 switch($type){
 
+//JSON返回
 case 'json':
 	header('Content-type:text/json');
-	die(json_encode(['pic'=>$pic));
+	die(json_encode(['pic'=>$pic . $_GET["type"]]));
 
 default:
-	die(header("Location: $pic"));
+	die(header("Location: $pic" . $_GET["type"]));
 }
